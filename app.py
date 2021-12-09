a515fe65aad17e3d9a59b78a72ee50eeea93138e
Maksym <mmsli@icloud.com>
2021-12-10 00:57:13 +0200

    all done with minor bugs

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template, send_from_directory, redirect, abort
-from functions import get_list_from_json, cut_50_symbol, add_count_comments, do_title_name, search_post_by_word
+from functions import get_list_from_json, cut_50_symbol, add_count_comments, do_title_name, search_post_by_word, sort_post_by_poster_name, add_tag_link, add_value, seach_post_by_tag
 
 DATA_PATH = "data/data.json"
 COMMENTS_PATH = "data/comments.json"
@@ -11,21 +11,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def page_index():
-    data = cut_50_symbol(do_title_name(get_list_from_json(DATA_PATH)))
+    data = cut_50_symbol(do_title_name(add_tag_link(get_list_from_json(DATA_PATH))))
     comments = get_list_from_json(COMMENTS_PATH)
     add_count_comments(data, comments)
-    return render_template('index.html',  data = data)
+    bookmarks_list = get_list_from_json(BOOKMARKS_PATH)
+    return render_template('index.html',  data = data, count_bookmarks = len(bookmarks_list))
 
 
-@app.route('/post/<int:id>/')
-def page_candidate(id):
-    data = do_title_name(get_list_from_json(DATA_PATH))
+@app.route('/post/<int:id>/', methods = ["POST", "GET"])
+def page_post(id):
+    data = do_title_name(add_tag_link(get_list_from_json(DATA_PATH)))
     comments = get_list_from_json(COMMENTS_PATH)
     add_count_comments(data, comments)
 
     for i in data:
         if id == i["pk"]:
-            postid = i["pk"]
+            postid = i.get("pk")
             username = i.get("poster_name")
             avatar = i.get("poster_avatar")
             picture = i.get("pic")
@@ -33,9 +34,30 @@ def page_candidate(id):
             views_count = i.get("views_count")
             count_comments = i.get("count_comments")
 
+            if request.method == "POST":
+
+                name = request.form.get("name_commentator")
+                comment = request.form.get("comment")
+
+                if name and comment:
+
+                    comments = get_list_from_json(COMMENTS_PATH)
+
+                    dict_comment = {
+                                        "post_id": postid,
+                                        "commenter_name": name,
+                                        "comment": comment,
+                                        "pk": len(comments) + 1
+                                    }
+
+                    comments.append(dict_comment)
+                    add_value(comments, COMMENTS_PATH)
+
+                abort(400)
+
             return render_template('post.html', username = username, avatar = avatar, picture=picture, content = content,
-                                  views_count = views_count, count_comments = count_comments, postid = postid,
-                                   comments = comments)
+                                      views_count = views_count, count_comments = count_comments, postid = postid,
+                                       comments = comments)
 
     abort(404)
 
@@ -47,7 +69,7 @@ def page_search():
         abort(404)
 
     else:
-        search_posts = search_post_by_word(cut_50_symbol(do_title_name(get_list_from_json(DATA_PATH))), search_word)
+        search_posts = search_post_by_word(add_tag_link(cut_50_symbol(do_title_name(get_list_from_json(DATA_PATH)))), search_word)
         comments = get_list_from_json(COMMENTS_PATH)
         add_count_comments(search_posts, comments)
 
@@ -55,6 +77,65 @@ def page_search():
         return render_template("search.html", search_posts = search_posts, count_posts = len(search_posts), search_word = search_word)
 
 
+@app.route('/users/<username>')
+def page_user_post(username):
+    data = cut_50_symbol(do_title_name(add_tag_link(get_list_from_json(DATA_PATH))))
+    comments = get_list_from_json(COMMENTS_PATH)
+    add_count_comments(data, comments)
+    data = sort_post_by_poster_name(data, username)
+
+    if len(data) >= 1:
+        return render_template('user-feed.html', data = data, username = username)
+
+    abort(404)
+
+
+@app.route('/bookmarks/add/<int:postid>')
+def add_post_to_bookmarks(postid):
+    data = cut_50_symbol(do_title_name(add_tag_link(get_list_from_json(DATA_PATH))))
+    comments = get_list_from_json(COMMENTS_PATH)
+    add_count_comments(data, comments)
+    bookmarks_list = get_list_from_json(BOOKMARKS_PATH)
+
+    for dict in data:
+        if postid == dict.get("pk"):
+            if dict not in bookmarks_list:
+                bookmarks_list.append(dict)
+                add_value(bookmarks_list, BOOKMARKS_PATH)
+
+                return redirect("/", code = 302)
+
+    abort(400)
+
+
+@app.route('/bookmarks/remove/<int:postid>')
+def del_post_from_bookmarks(postid):
+
+    bookmarks_list = get_list_from_json(BOOKMARKS_PATH)
+
+    for dict in bookmarks_list:
+        if postid == dict.get("pk"):
+            bookmarks_list.remove(dict)
+            add_value(bookmarks_list, BOOKMARKS_PATH)
+            return redirect("/", code = 302)
+
+    abort(400)
+
+
+@app.route("/bookmarks")
+def page_bookmarks():
+    bookmarks_list = get_list_from_json(BOOKMARKS_PATH)
+    return render_template('bookmarks.html',  bookmarks = bookmarks_list)
+
+@app.route('/tag/<tagname>')
+def page_search_tag(tagname):
+    data_list = cut_50_symbol(do_title_name(add_tag_link(get_list_from_json(DATA_PATH))))
+    comments = get_list_from_json(COMMENTS_PATH)
+    add_count_comments(data_list, comments)
+    data = seach_post_by_tag(data_list, tagname)
+    if len(data) >= 1:
+        return render_template('tag.html', data = data, tag = tagname)
+    abort(404)
 
 
 app.run()
--- functions.py
+++ functions.py
@@ -87,9 +87,68 @@ def search_post_by_word(list, word ):
     return ten_post_list
 
 
+def sort_post_by_poster_name(data, poster_name):
+    """
+    функция фильтрует и возвращает список только с определенным poster_name
+    :param data: list
+    :param poster_name: str
+    :return: list
+    """
+    data_new = []
+    for dict in data:
+        if dict["poster_name"].lower() == poster_name:
+            data_new.append(dict)
+
+    return data_new
+
+def add_tag_link(data):
+    for dict in data:
+        text = dict["content"].split(" ")
+        for j, word in enumerate(text):
+            if word.startswith("#"):
+                word = word.replace("#", "")
+                text[j] = f'<a href="tag/{word}">#{word}</a>'
+
+        dict["content"] = " ".join(text)
+
+    return data
+
+
+def add_value(list, jsonfile):
+    """
+    функция перезаписывает json файл
+    :param dict:
+    :param json:
+    :return: bool
+    """
+    with open(jsonfile, "w") as f:
+        json.dump(list, f, ensure_ascii=False, indent=4)
+
+    return True
+
+
+def seach_post_by_tag(data, tag):
+    """
+    функция которая формирует список словарей только если dict["content"] есть тег
+    :param data:
+    :param tag:
+    :return:
+    """
+    new_data = []
+    for dict in data:
+        if tag in dict["content"]:
+            new_data.append(dict)
+
+    return new_data
+
+
 #print(add_count_comments(get_list_from_json("data/data.json"), get_list_from_json("data/comments.json")))
 #print(get_true_word_form(1001))
 #a = "На следующий день"
-
 #print(search_post_by_word((get_list_from_json("data/data.json")), a))
 
+#print(sort_post_by_poster_name(get_list_from_json("data/data.json"), "leo"))
+
+#print(add_tag_link(get_list_from_json("data/data.json")))
+
+#print(seach_post_by_tag(get_list_from_json("data/data.json"), "днем"))
